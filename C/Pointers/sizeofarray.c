c3ca23f97b3f1668c49fb107bb2ddc7f64cef9fb
IanoNjuguna <inc3099@gmail.com>
2023-03-07 22:28:37 +0300

    Print the size of an array

---
+++ C/Pointers/sizeofarray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the sizeof an array
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int array[10];
+
+	printf("sizeof array: %lu\n", sizeof(array));
+	return (0);
+}
+
