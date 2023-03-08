11467495c21ea4bc27d43df1c473f0a7bc025316
IanoNjuguna <inc3099@gmail.com>
2023-03-08 16:41:08 +0300

    Print the sizeof an array

--- C/Pointers/addressofarray.c
+++ C/Pointers/addressofarray.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the address of an array
+ * main - print the address of an array && the sizeof an array
  *
  * Return: Always 0 (Success)
  */
@@ -12,6 +12,8 @@ int main(void)
 
 	printf("b: %p\n", (void *) b);
 	printf("&b: %p\n", (void *) &b);
+	printf("sizeof(b): %lu\n", sizeof(b));
+	printf("sizeof(&b): %lu\n", sizeof(&b));
 
 	return (0);
 }
