7a2d82a4c960109e1f57014ee457d6d3ce77b486
IanoNjuguna <inc3099@gmail.com>
2023-03-09 15:53:03 +0300

    int type to chop floats

---
+++ C/Working-Code/print_int.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints an int
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 89.765;
+	unsigned long int z = 45.234;
+	unsigned int b = 76.543;
+
+	printf("%d\n%u\n%lu\n", a, b, z);
+	return (0);
+}
+
